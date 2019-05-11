e2218606aad710f5091e29b2d870671b196d2dc9
Manny Vaccaro <emmanuel.vaccaro@gmail.com>
2019-05-11 14:54:35 +1000

    initial commit

---
+++ Assets/Prefabs/Player/Scripts/IKCharacter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKCharacter : MonoBehaviour
+{
+    public float globalIKWeight;
+
+    [Header("IK Elbow")]
+    public Transform leftElbowHint;
+    public Transform rightElbowHint;
+    public Transform lookAtHint;
+
+    [Header("IK Hand")]
+    public float leftElbowHintWeight;
+    public float rightElbowHintWeight;
+    public Transform leftHandTarget;
+    public Transform rightHandTarget;
+
+    [Header("IK Look")]
+    public float lookIKWeight;
+    public float bodyWeight;
+    public float headWeight;
+    public float eyesWeight;
+    public float clampWeight;
+
+    [Header("References")]
+    public Animator anim;
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetLookAtWeight(lookIKWeight * globalIKWeight, 
+                             bodyWeight * globalIKWeight, 
+                             headWeight * globalIKWeight, 
+                             eyesWeight * globalIKWeight, 
+                             clampWeight * globalIKWeight);
+
+        anim.SetLookAtPosition(lookAtHint.position);
+
+        // Hand IK
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, globalIKWeight);
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, globalIKWeight);
+
+        anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTarget.position);
+        anim.SetIKPosition(AvatarIKGoal.RightHand, rightHandTarget.position);
+
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, globalIKWeight);
+        anim.SetIKRotationWeight(AvatarIKGoal.RightHand, globalIKWeight);
+
+        anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTarget.rotation);
+        anim.SetIKRotation(AvatarIKGoal.RightHand, rightHandTarget.rotation);
+
+        // Elbow IK
+        anim.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, leftElbowHintWeight * globalIKWeight);
+        anim.SetIKHintPositionWeight(AvatarIKHint.RightElbow, rightElbowHintWeight * globalIKWeight);
+
+        anim.SetIKHintPosition(AvatarIKHint.LeftElbow, leftElbowHint.position);
+        anim.SetIKHintPosition(AvatarIKHint.RightElbow, rightElbowHint.position);
+    }
+}
---
+++ Assets/Prefabs/Player/Scripts/Player.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float runSpeed = 8f;
+    public float walkSpeed = 6f;
+    public float gravity = -10f;
+    public float jumpHeight = 15f;
+
+    [Header("Dash")]
+    public float dashSpeed = 50f;
+    public float dashDuration = .5f;
+    
+    private CharacterController controller; // Reference to CharacterController
+    private Vector3 motion; // Is the movement offset per frame
+    private bool isJumping;
+    private float currentJumpHeight, currentSpeed;
+
+    private void OnValidate()
+    {
+        currentJumpHeight = jumpHeight;
+        currentSpeed = walkSpeed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        // Set intial states
+        currentSpeed = walkSpeed;
+        currentJumpHeight = jumpHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get W, A, S, D or Left, Right, Up, Down Input
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        // Left Shift Input
+        bool inputRun = Input.GetKeyDown(KeyCode.LeftShift);
+        bool inputWalk = Input.GetKeyUp(KeyCode.LeftShift);
+
+        // Spacebar Input 
+        bool inputJump = Input.GetButtonDown("Jump");
+
+        if (inputRun)
+        {
+            currentSpeed = runSpeed;
+        }
+        if (inputWalk)
+        {
+            currentSpeed = walkSpeed;
+        }
+        
+        // Move character motion with inputs
+        Move(inputH, inputV);
+        // Is the Player grounded?
+        if (controller.isGrounded)
+        {
+            // Cancel gravity
+            motion.y = 0f;
+            // Pressing Jump Button?
+            if (inputJump)
+            {
+                Jump(jumpHeight);
+            }
+            if (isJumping)
+            {
+                // Make the Player Jump!
+                motion.y = currentJumpHeight;
+                // Reset back to false
+                isJumping = false;
+            }
+        }
+        // Apply gravity
+        motion.y += gravity * Time.deltaTime;
+        // Move the controller with motion
+        controller.Move(motion * Time.deltaTime);
+    }
+
+
+    // Move the character's motion in direction of input
+    void Move(float inputH, float inputV)
+    {
+        // Generate direction from input
+        Vector3 direction = new Vector3(inputH, 0f, inputV);
+
+        // Convert local space to world space direction
+        direction = transform.TransformDirection(direction);
+
+        // check if direction exceeds magnitude of 1
+        if (direction.magnitude > 1f)
+        {
+            // normalise
+            direction.Normalize();
+        }
+        // Apply motion to only X and Z
+        motion.x = direction.x * currentSpeed;
+        motion.z = direction.z * currentSpeed;
+    }
+
+    public void Jump(float height)
+    {
+        isJumping = true; // tell controller to jump at the right time
+        currentJumpHeight = height;
+
+    }
+
+    public void Dash()
+    {
+        StartCoroutine(SpeedBoost(dashSpeed, walkSpeed, dashDuration));
+    }
+
+    public IEnumerator SpeedBoost(float startSpeed, float endSpeed, float duration)
+    {
+        currentSpeed = startSpeed;
+
+        yield return new WaitForSeconds(duration);
+
+        currentSpeed = endSpeed;
+    }
+}
